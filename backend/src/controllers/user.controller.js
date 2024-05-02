894bd4d08c8a9ff62603fc5ba8885be48b265a64
abhijit kumar singh <abhijitkumarsingh.cse58@gmail.com>
2024-05-02 07:54:55 +0530

    verify user identity to grant access to secure resources

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,19 +1,64 @@
 import bcrypt from "bcrypt"
-
 import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.util.js";
 
 export const SIGNUPUSER = async (request, response) => {
-try {
-    const { name, username, email, password } = request.body;
+    try {
+        // get user details from frontend
+        const { name, username, email, password } = request.body;
 
-    // check either username field or email field in the user document matches the username or email provided 
-    const existedUser = await User.findOne({
-        $or: [{ username }, { email }]
-    })
+        // validation - not empty
+        if ([name, username, email, password].some((field) => field?.trim() === "")) {
+            return response.status(400).json({ message: "All fields are required" });
+        }
+        
+        // hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        // check either username field or email field in the user document matches the username or email provided 
+        const existedUser = await User.findOne({
+            $or: [{ username }, { email }]
+        })
+        
+        // check if user already exist: username or email
+        if (existedUser) {
+            return response.status(409).json({ message: "User with email or username already exist" })
+        }
 
-    if (existedUser) {
-        return response.status(409).json({ message: "User with email or username already exist" })
-    }
+        // check for images, avatar
+        const avatarLocalPath = request.files?.avatar[0]?.path
+        console.log("backend :: src :: controllers :: user.controller.js :: SIGNUPUSER: ", request.files);
+        if (!avatarLocalPath) {
+            return response.status(400).json({ message: "Avatar is required" });
+        }
+
+        // upload avatar to cloudinary
+        const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+        if (!avatar) {
+            return response.status(400).json({ message: "Avatar is required" });
+        }
+
+        // create the user and save it to db
+        const user = await User.create({
+            name,
+            avatar: avatar.url,
+            email, 
+            password,
+            username: username.toLowerCase()
+        })
+
+        // check if user is created and saved to db
+        const createdUser = await User.findById(user._id).select(
+            "-password -refreshToken"
+        )
+
+        if (!createdUser) {
+            return response.status(500).json({ message: "Something went wrong while registering the user" })
+        }
+
+        return response.status(201).json({ message: "User register successfully" })
+    
 } catch (error) {
     return response.status(500).json({ message: "Error while signing up" })
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
 
+import { SIGNUPUSER } from "../controllers/user.controller.js";
+
+import { upload } from "../middleware/multer.middleware.js";
+
 const router = Router();
+
+router.route("/signup").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }
+    ]),
+    SIGNUPUSER
+);
 
 export default router;
